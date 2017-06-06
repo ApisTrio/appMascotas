77173de95cf5d70a7a0190f3a19985f105f03d9a
Xavier Arias <xarias13@gmail.com>
2017-06-06 12:24:10 -0400

    modificaciones al registro

--- app/controllers/registro.js
+++ app/controllers/registro.js
@@ -3,39 +3,42 @@ angular.module("mascotas")
 .controller("registroController", [function () {
 
     var cdx = this;
-    
-    cdx.avanzar = function(valido){
-        
-        if(valido){
-            
+
+    cdx.avanzar = function (valido) {
+
+        if (valido) {
+
             cdx.pasos = cdx.pasos + 1;
         }
-        
-        
+
+
     }
-    
-    
+
     cdx.hoy = new Date();
-    
+
     cdx.datosDatepicker = {
-        
+
         meses: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-        mesesCorto:  ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+        mesesCorto: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
         diasSemana: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
         diasSemanaCorto: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
         hoy: 'Hoy',
         limpiar: 'Limpiar',
         cerrar: 'Cerrar',
         min: (new Date(cdx.hoy.getTime() - (1000 * 60 * 60 * 24 * 15))).toISOString(),
-        max: (new Date(cdx.hoy.getTime() + (1000 * 60 * 60 * 24 ))).toISOString()
-        
+        max: (new Date(cdx.hoy.getTime() + (1000 * 60 * 60 * 24))).toISOString()
+
     }
+
+    cdx.datos = {
+        
+        duenos: []
+    };
     
     
-
-   
     
-
-
+     
+    
+    
 
 }])
